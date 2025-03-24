8cd68f3592d048d71e511a12f6ae1af20f28ca9f
croxxxcs <42bratyxa@gmail.com>
2025-03-24 23:51:31 +0300

    added all WTF stats

--- Game.py
+++ Game.py
@@ -1,12 +1,38 @@
 import random
 
 WTF_factor = random.randint(1,3) 
-
 #character stats
 name = input('enter ur hero\'s name >> ')
 
-def print_stats(hp,dmg,inv):
+def print_stats(hp,dmg,inv,shield,):
+    print(f'the {name}\'s stats:')
     print(f'{name}\'s hp is {hp}')
     print(F'{name}\'s dmg is {dmg}')
     print(f'{name}\'s inventory capacity is {inv} ')
+    if WTF_factor == 2 or WTF_factor == 3:
+        print(f'because u are not a child u can bring a shield with you \n shield\'s toughness is {shield}')
 
+
+if WTF_factor == 1:
+    print(f'for an undescribeble pity, {name} is a little child')
+    hp1 = 100
+    dmg1 = 25
+    inv1 = 2
+    shield1 = 0
+    print_stats(hp1,dmg1,inv1,shield1)
+
+if WTF_factor == 2:
+    print(F'{name} is base adult knight')
+    hp2 = 175
+    dmg2 = 75
+    inv2 = 4
+    shield2 = 50
+    print_stats(hp2,dmg2,inv2,shield2)
+
+if WTF_factor == 3:
+    print(f'for an undiscribeble luck, {name} is caster')
+    hp3 = 150
+    dmg3 = 150
+    inv3 = 5
+    shield3 = 150
+    print_stats(hp3,dmg3,inv3,shield3)
