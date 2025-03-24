febba0cb2bc0273c5944e46615dbe28c84652276
croxxxcs <42bratyxa@gmail.com>
2025-03-24 22:48:21 +0300

    added WTF factor and first func.

--- Game.py
+++ Game.py
@@ -1,1 +1,12 @@
 import random
+
+WTF_factor = random.randint(1,3) 
+
+#character stats
+name = input('enter ur hero\'s name >> ')
+
+def print_stats(hp,dmg,inv):
+    print(f'{name}\'s hp is {hp}')
+    print(F'{name}\'s dmg is {dmg}')
+    print(f'{name}\'s inventory capacity is {inv} ')
+
